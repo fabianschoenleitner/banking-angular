0884ba8dd935f0af71b289733fab5fbf66c4eaed
Alphabetisierungsrate <s1045570@stud.sbg.ac.at>
2020-12-19 00:39:57 +0100

    Changed darkmode after themingService didn't work. To enable use the slider in the navbar, it should only be visible if the @include for light-theme is uncommented in styles.scss to not disturb my colleagues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,20 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Testing';
+  mode: boolean;
 
   constructor() {
+  }
+
+  changeMode(toMode: boolean): void {
+    this.mode = toMode;
+    const body = document.getElementsByTagName('body')[0];
+    if (toMode) {
+      body.classList.add('dark-theme');
+      body.classList.remove('light-theme');
+    } else {
+      body.classList.add('light-theme');
+      body.classList.remove('dark-theme');
+    }
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
 import {AuthService} from '../services/auth-service';
 import {BreakpointObserver} from '@angular/cdk/layout';
+import {EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +9,10 @@ import {BreakpointObserver} from '@angular/cdk/layout';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Output() themeSwitch = new EventEmitter<boolean>();
   collapsed = true;
   mobile = false;
+  currentMode = false;
 
   constructor(public authService: AuthService, public breakpointObserver: BreakpointObserver) {
   }
@@ -28,5 +31,14 @@ export class NavbarComponent implements OnInit {
 
   async onLogout(): Promise<void> {
     await this.authService.logout();
+  }
+
+  switchTheme(): void {
+    if (this.currentMode) {
+      this.currentMode = false;
+    } else {
+      this.currentMode = true;
+    }
+    this.themeSwitch.emit(this.currentMode);
   }
 }
